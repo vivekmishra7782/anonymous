b487769552b7fc670208eae11f7ccdba1fe19d17
vivek kumar mishra <109455833+vivekmishra7782@users.noreply.github.com>
2022-09-11 14:25:56 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# anonymous
+### Best Smart Voice Chat Music Robot For Telegram Groups ...
+
+
+<p align="center"><a href="https://t.me/i_anonymouss"><img src="https://i.ytimg.com/vi/aP6_Q6lElJE/maxresdefault.jpg"></a></p>
+
+
+
+
+### Deploy To Railway
+
+[![Deploy+on+Railway](https://railway.app/button.svg)](https://railway.app/new/template?template=https://github.com/Kuhmarvivek/anonymous&envs=API_ID,API_HASH,BOT_TOKEN,STRING_SESSION)
+
+
+### Deploy To Heroku
+
+[![Deploy+On+Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/Kuhmarvivek/anonymous)
+
+
+
+### Get String Session
+
+[![GenerateString](https://img.shields.io/badge/repl.it-generateString-yellowgreen)](https://replit.com/@i_anonymous/StringSession)
+
+
+
+#### Copy Pasters You Can Copy This But Must Give Credits ...
+
+### Owner Of This Repository![my musicbot](https://user-images.githubusercontent.com/109455833/182102057-d9050afa-a072-4a21-9f59-22a743a09c90.png)
+
+[![kumar vivekanand](https://telegra.ph//file/6c3966bce3b511c455f60.jpg)](https://t.me/i_anonymouss)
---
+++ plugins/alive.py
@@ -0,0 +1,91 @@
+import asyncio
+from time import time
+from datetime import datetime
+from modules.helpers.filters import command
+from modules.helpers.command import commandpro
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+    
+   
+
+@Client.on_message(command("start") & filters.private & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://i.ytimg.com/vi/aP6_Q6lElJE/maxresdefault.jpg",
+        caption=f"""**━━━━━━━━━━━━━━━━━━━━━━━━
+💥 ʜᴇʟʟᴏ, ɪ ᴀᴍ sᴜᴘᴇʀ ғᴀsᴛ ᴠᴄ ᴘʟᴀʏᴇʀ
+ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs ...
+┏━━━━━━━━━━━━━━━━━┓
+┣★ ᴄʀᴇᴀᴛᴏʀ : [vivek mishra](https://t.me/i_anonymouss)
+┣★ ᴜᴘᴅᴀᴛᴇs : [vivek mishra](https://t.me/i_anonymouss)
+┣★ sᴜᴘᴘᴏʀᴛ : [vivek mishra](https://t.me/kingofbilionsherts)
+┣★ sᴏᴜʀᴄᴇ › : [ɢᴇᴛ ʀᴇᴘᴏ ʜᴇʀᴇ](https://github.com/Kuhmarvivek/anonymous)
+┗━━━━━━━━━━━━━━━━━┛
+
+💞 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ
+ᴅᴍ ᴛᴏ ᴍʏ [ʟᴇɢᴇɴᴅ ᴏᴡɴᴇʀ](https://t.me/i_anonymouss) ...
+━━━━━━━━━━━━━━━━━━━━━━━━**""",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "➕ ❰ ᴊᴏɪɴ ʜᴇʀᴇ ғᴏʀ ᴜᴘᴅᴀᴛᴇs ❱ ➕", url=f"https://t.me/kingofbilionsherts")
+                ]
+                
+           ]
+        ),
+    )
+    
+    
+@Client.on_message(commandpro(["/start", "/alive", "vivekmishra"]) & filters.group & ~filters.edited)
+async def start(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://i.ytimg.com/vi/aP6_Q6lElJE/maxresdefault.jpg",
+        caption=f"""""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "💥 ᴊᴏɪɴ ʜᴇʀᴇ ᴀɴᴅ sᴜᴘᴘᴏʀᴛ 💞", url=f"https://t.me/kingofbilionsherts")
+                ]
+            ]
+        ),
+    )
+
+
+@Client.on_message(commandpro(["repo", "#repo", "@repo", "/repo", "source"]) & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://i.ytimg.com/vi/aP6_Q6lElJE/maxresdefault.jpg",
+        caption=f"""""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "💥 ᴄʟɪᴄᴋ ᴍᴇ ᴛᴏ ɢᴇᴛ ʀᴇᴘᴏ 💞", url=f"https://github.com/Kuhmarvivek/anonymous")
+                ]
+            ]
+        ),
+    )
